75bc2270eda9ae1b21a1e303e52baa3a4eef3c77
Karma-20056 <987166@win.edu.au>
2025-05-19 02:35:07 +1100

    fix the login page

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 session_start();
 if (isset($_SESSION['user_id'])) {
     if ($_SESSION['role'] === 'admin') {
-        header("Location: /pages/admin_dashboard.php");
+        header("Location: /MIT-122--Team-Ace/pages/admin_dashboard.php");
     } else {
-        header("Location: /pages/dashboard.php");
+        header("Location: /MIT-122--Team-Ace/pages/dashboard.php");
     }
 } else {
-    header("Location: /pages/login.php");
+    header("Location: /MIT-122--Team-Ace/pages/login.php");
 }
 exit;
 
--- pages/login.php
+++ pages/login.php
@@ -1,8 +1,6 @@
-<?php
-include '../includes/db.php';
-?>
+<?php include '../includes/db.php'; ?>
 <h2>Login</h2>
-<form action="../process/login_process.php" method="POST">
+<form action="login_process.php" method="POST">
     <input type="email" name="email" placeholder="Email" required><br>
     <input type="password" name="password" placeholder="Password" required><br>
     <button type="submit">Login</button>
